c3e5601e90a72fc50cc508199c7c016a15ade5f9
sadtowa <toni.afanasev.0505@mail.ru>
2024-05-29 06:28:34 +0500

    frame

--- RPNcalculator.cs
+++ RPNcalculator.cs
@@ -6,8 +6,58 @@ using System.Threading.Tasks;
 
 namespace CalculatorOnWPF
 {
-    internal class RPNcalculator
+    class Token
     {
 
+    }
+    
+    class Number : Token
+    {
+        double value;
+        public Number(double value)
+        {
+            this.value = value;
+        }
+    }
+
+    class Parenthesis : Token
+    {
+        bool isOpening;
+        public Parenthesis(bool isOpening)
+        {
+            this.isOpening = isOpening;
+        }
+    }
+
+    class Operation : Token
+    {
+        string value;
+        int priority;
+        public Operation(string value, int priority)
+        {
+            this.value = value;
+            this.priority = priority;
+        }
+    }
+
+    class Function : Token
+    {
+
+    }
+
+    class Variable : Token
+    {
+
+    }
+
+    internal class RPNcalculator
+    {
+        static List<Token> tokenize(string input)
+        { 
+            List<Token> tokens = new List<Token>();
+            
+            return tokens;
+        }
+        
     }
 }
